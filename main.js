e35bcb29c2e61477493d361edd6f09e7a8abf23a
Bartlomiej Zieba <bartlomiejzieba@gmail.com>
2019-06-11 18:38:45 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log ('Uszanowanko! Nieźle, że tu zaglądasz ;)');
+const name = 'Bartłomiej Zięba';
+const age = 29;
+console.log (name);
+console.log (age);
+console.log (`Nazywam się ${name} i mam ${age} lat`);
