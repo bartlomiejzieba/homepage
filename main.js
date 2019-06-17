7b86c04d82ffda7c62b9ef1ea55c4f99b40f8b61
Bartlomiej Zieba <bartlomiejzieba@gmail.com>
2019-06-17 21:22:36 +0200

    DOM changes in JS

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const age = 29;
 console.log (name);
 console.log (age);
 console.log (`Nazywam się ${name} i mam ${age} lat`);
+
+const heading = document.querySelector('.mainpage__header--js');
+console.log (heading.innerHTML);
+heading.innerHTML = 'Kurs WTF - Strona domowa (zmienione w JS!)'
+const paragraphs = document.querySelectorAll('p');
+console.log (paragraphs);
+console.log (paragraphs[3]);
