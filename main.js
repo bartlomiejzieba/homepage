d7b3e5bf7932fdafd4bef4255c4a99dbae29c9d5
Bartlomiej Zieba <bartlomiejzieba@gmail.com>
2019-06-25 22:50:54 +0200

    visual and JS changes

--- main.js
+++ main.js
@@ -11,3 +11,8 @@ paragraphForJs.innerHTML = 'paragraf dla JS <strong> (zmienione w JS!) </strong>
 const paragraphs = document.querySelectorAll('p');
 console.log (paragraphs);
 console.log (paragraphs[3]);
+
+const welcome = (name, age) => {
+    console.log(`Witaj ${name}, masz ${age} lat`)
+}
+welcome(`Bartek`, 29)
